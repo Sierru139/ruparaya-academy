9f8a1130ef8cab72f5c9f20d940df1445c2f8bf2
Isramm <muhisramyasim@gmail.com>
2024-10-10 18:03:15 +0800

    add

--- public/detailclass.php
+++ public/detailclass.php
@@ -10,6 +10,14 @@ $activeQna = array_filter($qna, function($item){
     return $item['qna-id'] == $num;
 });
 
+$teachdata = file_get_contents("./pages/data/teach.json");
+$teach = json_decode($teachdata, true);
+
+$activeTeach = array_filter($teach, function($item){
+    global $num;
+    return $item['teach-id'] == $num;
+});
+
 $classdata = file_get_contents("./pages/data/class.json");
 $class = json_decode($classdata, true);
 
@@ -70,80 +78,6 @@ $activeClass = array_filter($class, function($item){
     <!-- SECTION 7 QNA START ============= -->
     <!-- SECTION 7 QNA START ============= -->
     <!-- SECTION 7 QNA START ============= -->
-    
-    <!-- <section class="relative py-10">
-        <div class="absolute top-0 left-0 right-0  max-w-[1250px] -z-10 mx-auto">
-            <img src="./assets/pg1/bg-qna.png" class="mt-5 w-full h-full" alt="">
-        </div>
-        <div class="max-w-[1032px] mx-auto relative">
-            <div class="text-center">
-                <h1 data-aos="fade-up" data-aos-duration="1100" class="text-3xl font-bold mb-4 ">Pertanyaan Seputar <span class="color-blue-rpr-scn">Kelas 3D Modelling Product & Photoshoot</span></h1>
-            </div>
-            <div class="relative flex flex-wrap gap-5 justify-between w-full text-base max-md:mr-2.5 max-md:max-w-full">
-
-                <div class="mt-5 flex flex-wrap gap-10 items-start max-md:max-w-full">
-                    <div class="">
-                        <div class="ask w-full bg-gray-50 mb-4 shadow-md rounded-lg hdn">
-                            <div class="p-4 cursor-pointer flex justify-between relative rounded-t-lg">
-                                <h4 class="md:text-base text-sm font-medium">Apa itu Fullstack Web Development?</h4>
-                                <span class="arrow-rotate elementor-toggle-icon elementor-toggle-icon-right float-right text-right text-black md:w-4 w-2 absolute right-4" aria-hidden="true"><span class="elementor-toggle-icon-closed"><img src="../assets/icon/dropdown-plus.svg" alt=""></span></span>
-                            </div>
-                            <div class="answer w-full bg-gray-50 rounded-b-lg">                                
-                                <p class="md:text-base p-4 text-sm text-[#747474]">
-                                    <b>Fullstack Web Development</b> adalah pendekatan di mana seorang developer menangani pengembangan baik pada sisi frontend (antarmuka pengguna) menggunakan HTML, CSS, dan JavaScript, maupun pada sisi backend (server dan database) menggunakan teknologi seperti Node.js, Python, atau MySQL, untuk menciptakan aplikasi web yang berfungsi penuh dari tampilan hingga pengolahan data.
-                                </p>
-                            </div>
-                        </div>
-                        <div class="ask w-full bg-gray-50 mb-4 shadow-md rounded-lg hdn">
-                            <div class="p-4 cursor-pointer flex justify-between relative rounded-t-lg">
-                                <h4 class="md:text-base text-sm font-medium">Apa saja yang akan saya pelajari di kelas ini?</h4>
-                                <span class="arrow-rotate elementor-toggle-icon elementor-toggle-icon-right float-right text-right text-black md:w-4 w-2 absolute right-4" aria-hidden="true"><span class="elementor-toggle-icon-closed"><img src="../assets/icon/dropdown-plus.svg" alt=""></span></span>
-                            </div>
-                            <div class="answer w-full bg-gray-50 rounded-b-lg">                                
-                                <p class="md:text-base p-4 text-sm text-[#747474]">
-                                    Di kelas 3D Modelling Product & Photoshoot ini, Anda akan belajar teknik dasar hingga lanjutan dalam membuat model 3D produk, mulai dari konsep, pemodelan, tekstur, hingga rendering. Selain itu, Anda juga akan mempelajari cara melakukan photoshoot digital untuk produk-produk yang sudah di-render, mencakup pencahayaan, komposisi, dan editing post-produksi.
-                                </p>
-                            </div>
-                        </div>
-                        <div class="ask w-full bg-gray-50 mb-4 shadow-md rounded-lg hdn">
-                            <div class="p-4 cursor-pointer flex justify-between relative rounded-t-lg">
-                                <h4 class="md:text-base text-sm font-medium">Apakah saya perlu memiliki pengalaman sebelumnya?</h4>
-                                <span class="arrow-rotate elementor-toggle-icon elementor-toggle-icon-right float-right text-right text-black md:w-4 w-2 absolute right-4" aria-hidden="true"><span class="elementor-toggle-icon-closed"><img src="../assets/icon/dropdown-plus.svg" alt=""></span></span>
-                            </div>
-                            <div class="answer w-full bg-gray-50 rounded-b-lg">                                
-                                <p class="md:text-base p-4 text-sm text-[#747474]">
-                                    Tidak perlu. Kelas ini dirancang untuk pemula maupun mereka yang sudah memiliki sedikit pengalaman. Kami akan memandu Anda melalui setiap langkah, mulai dari pengenalan software hingga teknik yang lebih kompleks.
-                                </p>
-                            </div>
-                        </div>
-                        <div class="ask w-full bg-gray-50 mb-4 shadow-md rounded-lg hdn">
-                            <div class="p-4 cursor-pointer flex justify-between relative rounded-t-lg">
-                                <h4 class="md:text-base text-sm font-medium">Apakah ada dukungan setelah kelas selesai?</h4>
-                                <span class="arrow-rotate elementor-toggle-icon elementor-toggle-icon-right float-right text-right text-black md:w-4 w-2 absolute right-4" aria-hidden="true"><span class="elementor-toggle-icon-closed"><img src="../assets/icon/dropdown-plus.svg" alt=""></span></span>
-                            </div>
-                            <div class="answer w-full bg-gray-50 rounded-b-lg">                                
-                                <p class="md:text-base p-4 text-sm text-[#747474]">
-                                    Ya, setelah kelas selesai, Anda tetap mendapatkan akses ke komunitas online kami di mana Anda bisa bertanya, berbagi proyek, dan mendapatkan feedback dari mentor dan peserta lainnya. Selain itu, kami juga menyediakan sesi konsultasi terbatas untuk membantu jika ada tantangan yang dihadapi setelah kelas.
-                                </p>
-                            </div>
-                        </div>
-                        <div class="ask w-full bg-gray-50 mb-4 shadow-md rounded-lg hdn">
-                            <div class="p-4 cursor-pointer flex justify-between relative rounded-t-lg">
-                                <h4 class="md:text-base text-sm font-medium">Apa peralatan atau software yang dibutuhkan?</h4>
-                                <span class="arrow-rotate elementor-toggle-icon elementor-toggle-icon-right float-right text-right text-black md:w-4 w-2 absolute right-4" aria-hidden="true"><span class="elementor-toggle-icon-closed"><img src="../assets/icon/dropdown-plus.svg" alt=""></span></span>
-                            </div>
-                            <div class="answer w-full bg-gray-50 rounded-b-lg">                                
-                                <p class="md:text-base p-4 text-sm text-[#747474]">
-                                    Anda memerlukan laptop atau komputer dengan spesifikasi yang mendukung software 3D seperti Blender, Autodesk Maya, atau 3ds Max. Selain itu, untuk editing photoshoot, Anda bisa menggunakan software seperti Adobe Photoshop atau Lightroom. Kami akan memberikan panduan lengkap tentang instalasi dan penggunaan software selama kelas.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </section> -->
 
     <?php include_once __DIR__ . "/pages/sections/detailClass/qna.php" ?>
 
--- public/pages/sections/detailClass/teach.php
+++ public/pages/sections/detailClass/teach.php
@@ -1,19 +1,21 @@
+<?php foreach ($activeTeach as $teach) {?>
 <section class="flex">
     <div class="flex relative justify-end align-middle px-20 py-11 w-full  overflow-hidden min-h-[453px] max-md:px-5 max-md:max-w-full">
-        <img src="../assets/details/bg-instructor.jpg" class="object-cover absolute inset-0 size-full" alt="">
+        <img src="<?= $teach["bg"] ?>" class="object-cover absolute inset-0 size-full" alt="">
         <div data-aos="fade-left" data-aos-duration="1100" class="my-auto">
             <h2 class="relative text-xl font-medium text-white">Pengajar</h2>
-            <h1 class="relative mt-2.5 text-3xl font-bold text-white">Aliem Arif Perkasa</h1>
-            <p class="relative mt-2.5 mr-72 text-xl font-medium text-white max-md:mr-2.5">Manager Fullstack Web Development</p>
-            <p class="relative font-light mt-8 text-[16px] text-white w-[700px] max-md:max-w-full">Aliem Arif Perkasa adalah seorang Fullstack Web Developer berpengalaman. Dalam kelas ini, Aliem akan membimbing kamu menguasai front-end dan back-end development dengan pendekatan yang praktis dan mudah dipahami.</p>
+            <h1 class="relative mt-2.5 text-3xl font-bold text-white"><?= $teach["teacher"] ?></h1>
+            <p class="relative mt-2.5 mr-72 text-xl font-medium text-white max-md:mr-2.5"><?= $teach["job"] ?></p>
+            <p class="relative font-light mt-8 text-[16px] text-white w-[700px] max-md:max-w-full"><?= $teach["about"] ?></p>
             <div class="relative mt-12 max-md:mt-10">
                 <div class="flex">
-                    <a href="https://www.instagram.com/programer_malas?igsh=MW41dmM5cHhoNml4Yw%3D%3D">
+                    <a href="<?= $teach["linkIg"] ?>">
                         <img src="../assets/details/instagram 1.png" class="mr-2" alt="">
-                        <a href="https://www.instagram.com/programer_malas?igsh=MW41dmM5cHhoNml4Yw%3D%3D" class="text-white font-light" target="_blank">@programer_malas</a>
+                        <a href="<a href="<?= $teach["linkIg"] ?> class="text-white font-light text-lg" target="_blank"><?= $teach["usrnmIg"] ?></a>
                     </a>
                 </div>
             </div>
         </div>
     </div>
 </section>
+<?php } ?>
